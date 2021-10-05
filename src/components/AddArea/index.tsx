cab1d4f80df1758f1a1828bc4b3a6f55e2876375
gcascardi <guilhermecascardi@gmail.com>
2021-10-05 16:53:49 -0300

    first version

---
+++ src/components/AddArea/index.tsx
@@ -0,0 +1,28 @@
+import * as C from './styles';
+import { useState, KeyboardEvent } from 'react';
+
+type Props = {
+    onEnter: (taskName: string) => void
+}
+
+export const AddArea = ({ onEnter }: Props) => {
+    const [inputText, setInputText] = useState('');
+    const handleKeyUp = (e: KeyboardEvent) => {
+        if (e.code === "Enter" && inputText !== '') {
+            onEnter(inputText);
+            setInputText('');
+        }
+    }
+    return (
+        <C.Container>
+            <div className="image">➕</div>
+            <input
+                type="text"
+                placeholder="Add new task"
+                value={inputText}
+                onChange={e => setInputText(e.target.value)}
+                onKeyUp={handleKeyUp}
+            />
+        </C.Container>
+    );
+}
